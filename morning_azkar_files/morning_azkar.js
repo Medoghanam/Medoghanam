58ce7398f4b01e65caf2505d5cb329e3aa0bf966
Medoghanam <127614412+Medoghanam@users.noreply.github.com>
2024-12-30 07:08:26 +0200

    Add files via upload

--- morning_azkar_files/morning_azkar.js
+++ morning_azkar_files/morning_azkar.js
@@ -1,15 +1,3 @@
 function download(param) {
   alert('جارٍ التحميل...')
 }
-
-function copy(param) {
-  var copyText = document.getElementById("copyText");
-    copyText.select();
-    copyText.setSelectionRange(0, 99999);  // For mobile devices
-
-    // نسخ النص إلى الحافظة
-    document.execCommand("copy");
-
-    // إشعار للمستخدم
-    alert("تم نسخ النص: " + copyText.value);
-}
