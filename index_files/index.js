ee0261cddcd1b0b786128eedbf0feb1909522ed5
Medoghanam <127614412+Medoghanam@users.noreply.github.com>
2024-12-30 07:10:11 +0200

    Add files via upload

--- index_files/index.js
+++ index_files/index.js
@@ -5,3 +5,6 @@ function morning_azkar(param) {
 function evening_azkar(param) {
   alert('سيتم تحويلك الآن للبوت\nللقراءة\n/start-> أذكار المساء <- أذكار المسلم ')
 }
+function showRadio(url) {
+  document.getElementById('radioFrame').src = url;
+    }
